4c13f200f4d8a2228a13a95b7b92213c790ccf04
KodaiIkari <Ikari.Koudai@gmail.com>
2024-06-27 23:25:14 +0900

    ✨ add VDAE-9 : 選択肢の一覧を表示、選択するクラスを作成

---
+++ src/console/Selector.java
@@ -0,0 +1,36 @@
+package console;
+
+public class Selector {
+    private String[] options;
+    private int selectedIndex = 0;
+    ITerminalManager terminalManager;
+
+    public Selector(String[] options, ITerminalManager terminalManager) {
+        this.options = options;
+        this.terminalManager = terminalManager;
+    }
+
+    public void selectUp() {
+        terminalManager.clearScreen();
+        selectedIndex = (selectedIndex - 1 + options.length) % options.length;
+        viewOptions();
+    }
+
+    public void selectDown() {
+        terminalManager.clearScreen();
+        selectedIndex = (selectedIndex + 1) % options.length;
+        viewOptions();
+    }
+
+    public void viewOptions() {
+        terminalManager.clearScreen();
+        for (int i = 0; i < options.length; i++) {
+            if (i == selectedIndex) {
+                terminalManager.print(" > ");
+            } else {
+                terminalManager.print("   ");
+            }
+            terminalManager.printLine(options[i]);
+        }
+    }
+}
